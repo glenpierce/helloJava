9caed138b33a0f3dc9e8d62d2374a3ea4426e137
Glen Pierce <glen.piercev@gmail.com>
2019-02-13 11:17:00 -0800

    Tests. Fun experiment worked thanks: https://stackoverflow.com/questions/54663531/test-that-confirms-that-the-method-signature-has-not-been-changed

--- test/test/TestingCoercion.java
+++ test/test/TestingCoercion.java
@@ -1,26 +1,27 @@
 package test;
 
-import org.junit.Assert;
+import java.util.Arrays;
+
 import org.junit.Test;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
 
 public class TestingCoercion {
 
 	@Test
 	public void multiply2x2() {
 		MyClass myClass = new MyClass();
-		Assert.assertEquals(myClass.multiply(2, 2), 4);
+		assertEquals(myClass.multiply(2, 2), 4);
 	}
 
 	@Test
-	public void multiplyDoesNotWorkWithFloat() {
-		MyClass myClass = new MyClass();
+	public void multiplyOnlyWorksOnInts() {
 		try {
-			myClass.multiply(2.0, 2); //this code doesn't compile, but how can we test that the method signature hasn't changed?
-			fail("MyClass.multiply is only for integers");
-		} catch (Exception exception) {
-			Assert.assertTrue("MyClass.multiply correctly rejected a double", true);
+			MyClass.class.getMethod("multiply", int.class, int.class);
+			assertEquals(1, Arrays.stream(MyClass.class.getMethods()).filter(x -> x.getName().equals("multiply")).count());
+		} catch (NoSuchMethodException noSuchMethodException) {
+			// test has failed.
+			System.out.println(noSuchMethodException);
 		}
 	}
 
